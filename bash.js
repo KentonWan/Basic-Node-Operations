867edb990adcfa3f4df8027cc6e47c799e7df4ee
Kenton Wan <kenton.wan@gmail.com>
2018-08-01 10:27:16 -0700

    Basic Node Operations Assignment Completed

---
+++ bash.js
@@ -0,0 +1,10 @@
+const commands = require('./command.js');
+
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', (userInput) => {
+  userInput = userInput.toString().trim();
+
+
+  commands.evaluateCmd(userInput)
+});
